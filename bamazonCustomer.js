07689fe1a98bdc7bc9a7406b28ab946fa2f6d41f
unknown <kevhemsoth@gmail.com>
2019-04-15 19:06:55 -0400

    customer needs validation

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -31,39 +31,61 @@ function cusReq() {
       {
         type: 'number',
         name: 'id', 
-        message: 'What item would you like to buy? (ID)',
-      /*  validate: function (input) {
-          var done = this.async();
-          setTimeout(function() {
-            if (typeof input !== 'number') {
-              done('You need to provide a number');
-              return;
-            }
-            done(null, true);
-          }, 3000);
-          }*/
+        message: 'What item would you like to buy? (ID)'
         },
       {
         type: 'number', 
         name: 'amount', 
-        message: 'How many would you like to buy?',
-       /* validate: function (input) {
-          var done = this.async();
-          setTimeout(function() {
-            if (typeof input !== 'number') {
-              done('You need to provide a number');
-              return;
-            }
-            done(null, true);
-          }, 3000);*/
+        message: 'How many would you like to buy?'
           }
     ]).then(function(ans) {
-      console.log(ans);
-      connection.query('select item_id from products where ?', {item_id: ans.id}, function(err, res) {
+      connection.query('select stock_quantity from products where ?', {item_id: ans.id}, function(err, res) {
         if(err) throw(err);
-        
+        if (res[0].stock_quantity >= ans.amount) {
+          connection.query('update products set ? where ?',
+           [
+             {
+               stock_quantity: res[0].stock_quantity - ans.amount
+             },
+             {
+               item_id: ans.id
+             }
+           ]);
+          connection.query('select price from products where ?', {item_id: ans.id}, function(err, res) {
+            if(err) throw (err);
+            var price = res[0].price;
+            var totalPrice = price * ans.amount;
+            console.log("You're total comes to $" + totalPrice + '\n');
+            reset();
+          });
+        }
+          else if (res[0].stock_quantity < ans.amount) {
+            console.log('Insufficient quantity! We only have ' + res[0].stock_quantity + ' of those left!\n');
+            displayItems();
+          }
       })
     })
+};
+
+function reset() {
+  inquirer
+  .prompt([
+    {
+      type: 'list', 
+      name: 'choice', 
+      message: 'What would you like to do next?',
+      choices: ['Buy another item', 'Take my items and leave']
+    }
+  ]).then(function(ans) {
+    if (ans.choice === 'Buy another item') {
+      console.log('\n');
+      displayItems();
+    }
+    else {
+      console.log('Thanks for coming!')
+      connection.end();
+    }
+  })
 }
 
 
